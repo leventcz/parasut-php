4f8bdf775259144642928de54ecca8649d49f8fb
leventcorapsiz <leventcorapsiz@hotmail.com>
2023-10-06 00:29:50 +0300

    invalid argument type fixed

--- src/Resources/TrackableJob.php
+++ src/Resources/TrackableJob.php
@@ -4,14 +4,28 @@ declare(strict_types=1);
 
 namespace Leventcz\Parasut\Resources;
 
-use Leventcz\Parasut\Resources\Concerns\ShowsResource;
+use Leventcz\Parasut\Exceptions\ClientException;
+use Leventcz\Parasut\ValueObjects\Method;
 
 class TrackableJob extends ApiResource
 {
-    use ShowsResource;
-
     /**
      * @var string
      */
     protected string $resource = 'trackable_jobs';
+
+    /**
+     * @param  string  $id
+     * @return array<mixed>|null
+     * @throws ClientException
+     */
+    public function show(string $id): ?array
+    {
+        return $this
+            ->getHttpClient()
+            ->authenticatedRequest(
+                method: Method::GET,
+                uri: "{$this->getResource()}/$id"
+            );
+    }
 }
